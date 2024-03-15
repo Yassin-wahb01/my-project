34014b0192fd7c0085c4057cd3bae2e46e519731
Wahb <ym8142004@gmail.com>
2024-03-15 21:25:08 +0200

    Hello world function

---
+++ func.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void HW(int n) {
+	for (int i = 0; i <= n; i++)
+		cout << "Hello World !! \n";
+}
+
+
+void main() {
+
+	int n;
+	cin >> n;
+	HW(n);
+
+
+
+}
